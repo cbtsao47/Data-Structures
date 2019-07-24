d3a627199e913ae0b28f7488e1e999eee25bdfeb
Ben <cbtsao47@gmail.com>
2019-07-24 17:50:52 -0500

    complete BST

--- queue/queue.py
+++ queue/queue.py
@@ -1,3 +1,34 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
+class SinglyLinkedList:
+    def __init__(self, head=None, tail=None):
+        self.head = head
+        self.tail = tail
+        self.length = 0
+
+    def push(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return self
+
+    def pop(self, value):
+        if not self.head:
+            return None
+        current_tail = self.tail
+        while current_tail:
+            current_tail = current_tail.next
+
+
 class Queue:
     def __init__(self):
         self.size = 0
